11eb5d0276cdeef33a0719384f92f35b8c9df5bd
Kauan Cardoso <kauancardososilva@gmail.com>
2024-08-15 23:28:44 -0400

    [FEAT] Crud dos address

--- src/app/usecases/Address/CrudAddressAction.ts
+++ src/app/usecases/Address/CrudAddressAction.ts
@@ -10,9 +10,28 @@ interface Address {
     complement: string;
     userId: Promise<number>;
 }
-class CreateAddressAction{
-    public createAddress(address: Address){
-        return addressDao.createAddress(address);
+class CrudAddressAction{
+    public async createAddress(address: Address){
+        const response = await addressDao.createAddress(address);
+        if (response) {
+            return {message: "Endereço cadastrado com sucesso!"};
+        }
+        return {message: "Erro: Não foi possível cadastrar o endereço!"};
+    }
+    public async updateAddress(id:number, address: Address){
+        const response = await addressDao.updateAddress(id, address);
+        if (response) {
+            return {message: "Endereço atualizado com sucesso!"};
+        }
+        return {message: "Erro: Não foi possível atualizar o endereço!"};
     }   
+    public async deleteAddress(id:number){
+        const response = await addressDao.deleteAddress(id);
+        if (response) {
+            return {message: "Endereço deletado com sucesso!"};
+        }
+        return {message: "Erro: Não foi possível deletar o endereço!"};
+    }
+
 }
-export const createAddressAction = new CreateAddressAction();
+export const crudAddressAction = new CrudAddressAction();
